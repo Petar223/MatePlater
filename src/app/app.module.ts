742cb6d72ce1b7a330f3e61081137822a090b66a
Petar Stankovic <pstankovic@nanointeractive.com>
2023-01-31 20:11:01 +0100

    MATE_0001 adding signup, login, sidenav and toolbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,42 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import {MaterialModule} from "./material.module";
+import { FoodComponent } from './food/food.component';
+import { GroceriesComponent } from './food/groceries/groceries.component';
+import { TreningComponent } from './trening/trening.component';
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    FoodComponent,
+    GroceriesComponent,
+    TreningComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MaterialModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/trening/trening.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trening',
+  templateUrl: './trening.component.html',
+  styleUrls: ['./trening.component.css']
+})
+export class TreningComponent {
+
+}
